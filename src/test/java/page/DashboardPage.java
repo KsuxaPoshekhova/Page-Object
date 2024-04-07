07142d5912d7d00aa14844caeb03ef87a754e89a
Ksenia Poshekhova <kposehova@gmail.com>
2024-04-07 21:08:16 +0300

    Commit CardsTest

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -4,13 +4,14 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import data.DataHelper;
+import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
-    private final String balanceStart = " баланс: ";
+    private final String balanceStart = ", баланс: ";
     private final String balanceFinish = " p. ";
     private final SelenideElement heading = $("[data-test-id=dashboard]");
     private final ElementsCollection cards = $$(".list__item div");
@@ -19,28 +20,20 @@ public class DashboardPage {
         heading.shouldBe(visible);
     }
 
-    public int getCardBalance(String maskedCarNumber) {
-        var text = cards.findBy(Condition.text(maskedCarNumber)).getText();
-        return extractBalance(text);
-    }
-
-
-    public int getCardBalance(int index) {
-        var text = cards.get(index).getText();
-        return extractBalance(text);
-    }
-
-
     public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
         cards.findBy(Condition.attribute("data-test-id", cardInfo.getTestId())).$("button").click();
         return new TransferPage();
     }
 
+    public int getCardBalance(String maskedCarNumber) {
+        var text = cards.findBy(Condition.text(maskedCarNumber)).getText();
+        return extractBalance(text);
+    }
+
     private int extractBalance(String text) {
-        String balanceStart = " баланс: ";
-        var start = text.indexOf(balanceStart);
-        var finish = text.indexOf(balanceFinish);
-        var value = text.substring(start + balanceStart.length(), finish);
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
         return Integer.parseInt(value);
     }
 
--- src/test/java/page/VerificationPage.java
+++ src/test/java/page/VerificationPage.java
@@ -6,8 +6,8 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
-    private final SelenideElement codeField = $("[data-test-id=login] input");
-    private final SelenideElement verifyButton = $("[data-test-id=action-login]");
+    private final SelenideElement codeField = $("[data-test-id='code'] input");
+    private final SelenideElement verifyButton = $("[data-test-id='action-verify']");
 
     public VerificationPage() {
         codeField.shouldBe(visible);
